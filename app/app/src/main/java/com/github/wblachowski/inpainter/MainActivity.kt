d02c80100c2753e68ef3d927f88dc1fb71ccaec3
Wojciech Blachowski <wblachowsky@gmail.com>
2019-11-09 19:35:55 +0100

    Add selecting an image from gallery

--- app/app/src/main/java/com/github/wblachowski/inpainter/MainActivity.kt
+++ app/app/src/main/java/com/github/wblachowski/inpainter/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.github.wblachowski.inpainter
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore.Images.Media.getBitmap
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imgSelectButton.setOnClickListener { openImageSelectionIntent() }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == SELECT_IMG_REQUEST && resultCode == Activity.RESULT_OK) {
+            data?.data?.also { imageView.setImageBitmap(getBitmap(contentResolver, it)) }
+        }
+    }
+
+    private fun openImageSelectionIntent() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Select image"), SELECT_IMG_REQUEST)
+    }
+
+    companion object {
+        const val SELECT_IMG_REQUEST = 1
     }
 }
